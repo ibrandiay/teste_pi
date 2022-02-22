601b8e9f65c07ffdfc366cb872401b31cf6c601f
ibra ndiaye <baye102@hotmail.fr>
2022-02-22 10:40:07 +0000

    ff

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ while True :
     _ , frame = cap.read()
     if _ : 
         print('bon' ) 
-        cv2.imwrite("img.jpg" , frame[::, :: , ::-1] ) 
+        cv2.imwrite("img.jpg" , frame[::-1] ) 
     else : print("pas bien " ) 
 
