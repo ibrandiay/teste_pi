70dfe625110e90387a9c143cbd2067461f243427
ibra ndiaye <baye102@hotmail.fr>
2022-02-22 10:32:44 +0000

    dd

---
+++ test.py
@@ -0,0 +1,12 @@
+import cv2 
+
+
+# read cam 
+cap = cv2.VideoCapture(0)
+while True : 
+    _ , frame = cap.read()
+    if _ : 
+        print('bon' ) 
+        cv2.imwrite("img.jpg" , frame[::, :: , ::-1] ) 
+    else : print("pas bien " ) 
+
