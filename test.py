41330cdf57b3ee06b517e3a6b7fd6f1f6688f45a
ibra ndiaye <baye102@hotmail.fr>
2022-02-23 10:03:11 +0000

    gf

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ while True :
     _ , frame = cap.read()
     if _ : 
         print('bon' ) 
-        cv2.imwrite("img.jpg" , frame[::-1] ) 
+        cv2.imwrite("img.jpg" , cv2.cvtColor(frame , cv2.COLOR_BGR2RGB)) 
     else : print("pas bien " ) 
 
