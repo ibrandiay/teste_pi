ec682f88323cf7bf9350c29f692c414817e82303
ibra ndiaye <baye102@hotmail.fr>
2022-02-23 14:52:30 +0000

    ee

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ import cv2
 
 # read cam 
 cap = cv2.VideoCapture(0)
+writer= cv2.VideoWriter('video.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (1200,800))
 while True : 
     _ , frame = cap.read()
     if _ : 
-        print('bon' ) 
-        cv2.imwrite("img.jpg" , frame) 
+        print('bon' )
+        writer.write(frame)
+        #cv2.imwrite("img.jpg" , frame) 
     else : print("pas bien " ) 
 
