ae2abe7f2bb60ffdf6898af70ea4df6a547063cf
ibra ndiaye <baye102@hotmail.fr>
2022-02-24 13:28:33 +0000

    ee

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import cv2
 
 # read cam 
 cap = cv2.VideoCapture(0)
-w=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH ))
-h=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
-writer= cv2.VideoWriter('video1.avi', cv2.VideoWriter_fourcc(*'XVID'), 20, (w,h))
+#w=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH ))
+#h=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
+writer= cv2.VideoWriter('video1.avi', cv2.VideoWriter_fourcc(*'XVID'), 20, (1080,1920))
 while True : 
     _ , frame = cap.read()
     if _ : 
