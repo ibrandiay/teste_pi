d4931ac33cc00b57fc403c4902427a744cb48a08
ibra ndiaye <baye102@hotmail.fr>
2022-02-28 14:16:19 +0000

    ddd

--- size.py
+++ size.py
@@ -2,5 +2,5 @@ import cv2
 
 
 
-image = cv2.imread("img.jpg")
+image = cv2.imread("img1.jpg")
 print(image.shape)
--- test.py
+++ test.py
@@ -3,14 +3,14 @@ import cv2
 
 # read cam 
 cap = cv2.VideoCapture(0)
-w=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH ))
-h=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
+w=int(cap.set(cv2.CAP_PROP_FRAME_WIDTH , 1920 ))
+h=int(cap.set(cv2.CAP_PROP_FRAME_HEIGHT,  1080))
 writer= cv2.VideoWriter('video3.avi', cv2.VideoWriter_fourcc(*'XVID'), 20, (w,h))
 while True : 
     _ , frame = cap.read()
     if _ : 
         print('bon' )
         writer.write(frame)
-        #cv2.imwrite("img1.jpg" , frame) 
+        cv2.imwrite("img1.jpg" , frame) 
     else : print("pas bien " ) 
 
