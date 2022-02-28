f69561986dfcfbc446453f40e428f621766e5159
ibra ndiaye <baye102@hotmail.fr>
2022-02-28 10:20:02 +0000

    fg

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ import cv2
 cap = cv2.VideoCapture(0)
 #w=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH ))
 #h=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
-writer= cv2.VideoWriter('video1.avi', cv2.VideoWriter_fourcc(*'XVID'), 20, (1080,1920))
+writer= cv2.VideoWriter('video2.avi', cv2.VideoWriter_fourcc(*'XVID'), 20, (1080,1920))
 while True : 
     _ , frame = cap.read()
     if _ : 
         print('bon' )
         writer.write(frame)
-        cv2.imwrite("img.jpg" , frame) 
+        cv2.imwrite("img1.jpg" , frame) 
     else : print("pas bien " ) 
 
