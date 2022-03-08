4fe3d3d63e73da95a490fbfeb3acb8daba4b69db
ibrandiay <baye102@hotmail.fr>
2022-03-08 11:51:59 +0100

    bn

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import cv2
 
 # read cam 
 cap = cv2.VideoCapture(0)
-w=int(cap.set(cv2.CAP_PROP_FRAME_WIDTH  , 1080))
-h=int(cap.set(cv2.CAP_PROP_FRAME_HEIGHT , 1920 ))
+w=int(cap.set(cv2.CAP_PROP_FRAME_WIDTH  , 1920))
+h=int(cap.set(cv2.CAP_PROP_FRAME_HEIGHT , 1080 ))
 print("la taille " , w, h ) 
 writer= cv2.VideoWriter('video7.avi', cv2.VideoWriter_fourcc(*'XVID'), 20, (1080,1920))
 while True : 
