fc72905f49c471ee14fdaf818c13c1d5f23f7129
ibra ndiaye <baye102@hotmail.fr>
2022-03-08 15:05:52 +0000

    hgdf

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 w=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH  ))
 h=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
 print("la taille " , w, h ) 
-writer= cv2.VideoWriter('video7.avi', cv2.VideoWriter_fourcc(*'XVID'), 20, (w,h))
+writer= cv2.VideoWriter('video8.avi', cv2.VideoWriter_fourcc(*'XVID'), 20, (w,h))
 while True : 
     _ , frame = cap.read()
     if _ : 
