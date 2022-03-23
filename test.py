27730391c59908419f0e96fe33cdbee3d83f0f7a
ibra ndiaye <baye102@hotmail.fr>
2022-03-23 10:22:07 +0000

    dgg

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ cap = cv2.VideoCapture(0)
 w=int(cap.set(cv2.CAP_PROP_FRAME_WIDTH , 1920  ))
 h=int(cap.set(cv2.CAP_PROP_FRAME_HEIGHT , 1080))
 print("la taille " , w, h ) 
-writer= cv2.VideoWriter('video11.avi', cv2.VideoWriter_fourcc(*'XVID'), 25, (w,h))
+#writer= cv2.VideoWriter('video11.avi', cv2.VideoWriter_fourcc(*'XVID'), 25, (w,h))
 while True : 
     _ , frame = cap.read()
     if _ : 
         print('bon' )
-        writer.write(frame)
-        #cv2.imwrite("img_bon33.jpg" , frame) 
+        #writer.write(frame)
+        cv2.imwrite("img_bon223.jpg" , frame) 
     else : print("pas bien " ) 
 
