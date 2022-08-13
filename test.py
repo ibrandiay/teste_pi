ba689d4be96232dd14403a0afdbfee8544a36b00
ibra <baye102@hotmail>
2022-08-14 00:57:43 +0200

    teste modifié

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import cv2 
-
+import numpy as np
 
 # read cam 
 cap = cv2.VideoCapture(0)
-w=int(cap.set(cv2.CAP_PROP_FRAME_WIDTH , 1920  ))
-h=int(cap.set(cv2.CAP_PROP_FRAME_HEIGHT , 1080))
 print("la taille " , w, h ) 
 #writer= cv2.VideoWriter('video15.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (640,480))
 while True : 
